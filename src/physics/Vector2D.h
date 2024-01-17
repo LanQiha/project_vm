9a42e97e8685207dd00595fc42cc25dff753d38d
齐哈 <Cherazade1412@protonmail.com>
2024-01-17 15:41:16 +0100

    stuff

--- src/physics/Vector2D.h
+++ src/physics/Vector2D.h
@@ -27,11 +27,6 @@ class Vector2D
             return Vector2D(X * scalar, Y * scalar);
         }
 
-        inline Vector2D operator*(const double scalar)
-        const {
-            return Vector2D(X * scalar, Y * scalar);
-        }
-
         inline Vector2D operator*(const Vector2D& v2)
         const {
             return Vector2D(X*v2.X - Y*v2.Y, Y*v2.X + X*v2.Y);
